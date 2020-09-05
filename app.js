8e1615a8d8cbfa1a02ac36f308b420c6c8ede4f2
udara <udara@accusys.lk>
2020-09-05 13:03:13 +0530

    re structured

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
-var routes_v1 = require('./routes/route_v1');
-var routes_v2 = require('./routes/route_v2');
+
 
 // Start the server
 const PORT = 5000;
 const VERSION = 'v1';
+const ROUTE = '/api/'+VERSION;
 
+var appRoutes = require('./routes/route');
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res
       .status(200)
       .send('Smart Home System')
       .end();
 });
 
-app.use('/api/v1', routes_v1);
-app.use('/api/v2', routes_v2);
+app.use(ROUTE, appRoutes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
