fc45fa7ec7a86466986b922a0dc1f72bc450a00b
udarachamara <38930562+udarachamara@users.noreply.github.com>
2020-09-09 21:12:20 +0530

    Update db.config.js
    Commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 const db = {
     host: 'smart-home.ccmxwwzm0dff.us-east-2.rds.amazonaws.com',
     port: '3306',
-    user: 'admin',
-    password: 'UDarachama9465',
+    user: 'user',
+    password: 'password',
     database: 'smart-home'
 }
 module.exports = db;
